ce7cc0ec4ffed7ae191c0c9c7266ad7c6f54b2a4
Kishor Dhaduk <kdhaduk1207@gmail.com>
2023-03-15 06:40:23 +0530

    Changes related to Validations

--- src/app/city-detail/city-detail.component.ts
+++ src/app/city-detail/city-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'ci-city-detail',
@@ -9,19 +9,31 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class CityDetailComponent implements OnInit   {
  
   cityInfoForm : FormGroup;
+  showDetails : boolean = true;
   
   constructor(private formBuilder:FormBuilder)
   {
     this.cityInfoForm = this.formBuilder.group(
       {
-        name:'',
-        description:''
+        name:['',[Validators.required,Validators.maxLength(50)]],
+        description:['',[Validators.maxLength(255)]]
       }
     )  
   } 
  
   ngOnInit(): void {
     
+  }
+
+  save()
+  {
+      console.log(this.cityInfoForm);
+  }
+
+  clearValidation()
+  {
+     const nameControl = this.cityInfoForm.get('name');
+     nameControl?.clearValidators();
   }
 
 }
